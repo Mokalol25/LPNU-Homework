4a37a0e1e25b88329290b3ebdf996b4d0c223ec4
Mokalol25 <93078882+Mokalol25@users.noreply.github.com>
2026-02-25 10:31:04 +0200

    test

--- Lab-1/lab1.py
+++ Lab-1/lab1.py
@@ -1,5 +1,3 @@
-import unittest
-
 def zigzag_matrix(m, n):
     matrix = [[0] * n for _ in range(m)]
     value = 1
@@ -25,36 +23,3 @@ def zigzag_matrix(m, n):
                 b -= 1
 
     return matrix
-
-class TestZigzagMatrix(unittest.TestCase):
-
-    def check_matrix(self, m, n):
-        matrix = zigzag_matrix(m, n)
-
-        print(f"\nПеревірка для m={m}, n={n}")
-        for a in matrix:
-            print(a)
-
-    def test_1(self):
-        self.check_matrix(5, 5)
-
-    def test_2(self):
-        self.check_matrix(2, 4)
-
-    def test_3(self):
-        self.check_matrix(6, 1)
-
-    def test_4(self):
-        matrix = zigzag_matrix(1, 1)
-        print("\nПеревірка для m=1, n=1")
-        print(matrix)
-        self.assertEqual(matrix, [[1]])
-
-tester = TestZigzagMatrix()
-
-tester.test_1()
-tester.test_2()
-tester.test_3()
-tester.test_4()
-
-unittest.main(verbosity=2, exit=False)
